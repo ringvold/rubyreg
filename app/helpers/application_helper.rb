f2a138417a191a6d7f2b64b764f5b0067b565ed6
Harald Ringvold <harald.ringvold@gmail.com>
2012-04-22 07:41:22 +0200

    Changed menu link highlighting method to be based on current controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,13 @@ module ApplicationHelper
 	# Return "active" if the current path is the 
   # same as the path given as parameter
 	# Is used for the main menu
-	def is_active?(path)
-	  "active" if current_page?(path)
-	end	
+	# def is_active?(path)
+	#   "active" if current_page?(path)
+	# end	
 
 	# Alternative method. Checks only action name.
-	# def is_active?(page_name)
- 	#    "active" if params[:action] == page_name
- 	#  end
+	def is_active?(page)
+ 	   "active" if params[:controller] == page
+ 	 end
 
 end

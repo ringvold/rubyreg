ad379660d095c9ae5ea4d5f4e2e84f8470001755
Harald Ringvold <harald.ringvold@gmail.com>
2013-01-31 17:49:12 +0100

    first step on localizing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-	
-	attr_accessible :username, :email, :password, :password_confirmation, :role, :name
-	
-	authenticates_with_sorcery!	
-	validates :username, :name, :email, :password, :role, :presence => true
+
+	attr_accessible :email, :password, :password_confirmation, :role, :name
+
+	authenticates_with_sorcery!
+	validates :name, :email, :role, :presence => true
 	validates :password, :presence => true, :on => :create
-	validates :username, :uniqueness => true
+	# validates :username, :uniqueness => true
 	validates_confirmation_of :password, :message => "should match confirmation", :if => :password
-	
+
 end

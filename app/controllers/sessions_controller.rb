7f0cc740b6c1ecd5112b57395e51115b00a9e7a6
Harald Ringvold <harald.ringvold@gmail.com>
2013-02-21 20:48:35 +0100

    Fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = login(params[:username], params[:password], params[:remember_me])
+    user = login(params[:email], params[:password], params[:remember_me])
     if user
       redirect_back_or_to dashboard_url, :notice => "Innlogget!"
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
 
 	attr_accessible :email, :password, :password_confirmation, :role, :name
 
-	authenticates_with_sorcery!
 	validates :name, :email, :role, :presence => true
 	validates :password, :presence => true, :on => :create
 	# validates :username, :uniqueness => true
